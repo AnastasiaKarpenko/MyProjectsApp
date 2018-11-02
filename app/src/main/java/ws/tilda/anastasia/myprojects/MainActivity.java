0f643bb4e382b4748deec3da5509c765eb760ccb
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-02 16:53:38 +0200

    Set DataBinding in the MainActivity

--- app/src/main/java/ws/tilda/anastasia/myprojects/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/MainActivity.java
@@ -1,18 +1,24 @@
 package ws.tilda.anastasia.myprojects;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import ws.tilda.anastasia.myprojects.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     String[] projectList = {"Project1", "Project2", "Project3"};
 
+    ActivityMainBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         setProjectList();
     }
