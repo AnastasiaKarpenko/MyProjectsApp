12467ea564e0525f8dcceca63ee3a7bfac647f08
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-02 17:49:30 +0200

    Created a member variable of List of projects in the ViewModel class. Getting the adapter from the listView

--- app/src/main/java/ws/tilda/anastasia/myprojects/BindingUtils.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/BindingUtils.java
@@ -1,4 +1,18 @@
 package ws.tilda.anastasia.myprojects;
 
+import android.databinding.BindingAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
 public class BindingUtils {
+
+    @BindingAdapter("projectItem")
+    public static void populateProjectList(ListView listView, List<ProjectModel> list) {
+        if (list == null) {
+            return;
+        }
+        MainAdapter adapter = (MainAdapter) listView.getAdapter();
+    }
+
 }
--- app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
@@ -1,18 +1,19 @@
 package ws.tilda.anastasia.myprojects;
 
 import android.arch.lifecycle.ViewModel;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainViewModel extends ViewModel {
+
+    List<ProjectModel> projectsList = new ArrayList<>();
 
     public void setProjectList() {
 //        ListView projectListView = findViewById(R.id.listview);
 //        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, projectList);
 //        projectListView.setAdapter(arrayAdapter);
     }
-
-
 
 
 }
