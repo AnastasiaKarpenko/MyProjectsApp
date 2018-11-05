279a65f8093788ecc0a74baeeec70fbc13ea3aea
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-05 10:08:30 +0200

    Deleted unused object

--- app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
@@ -4,13 +4,10 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainViewModel extends AndroidViewModel {
     public final LiveData<List<ProjectModel>> projectsListings;
-
-    List<ProjectModel> projectsList = new ArrayList<>();
 
     MainViewModel(Application application) {
         super(application);
