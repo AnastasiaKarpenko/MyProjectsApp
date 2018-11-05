733122243c878f0d7b99d07d70b64fa873e349e5
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-05 10:07:28 +0200

    Edited MainViewModel class: - added LiveData final variable that holds the ProjectList received from the repository = added getter for project List observable data from the LiveData instance

--- app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
@@ -1,19 +1,25 @@
 package ws.tilda.anastasia.myprojects;
 
-import android.arch.lifecycle.ViewModel;
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainViewModel extends ViewModel {
+public class MainViewModel extends AndroidViewModel {
+    public final LiveData<List<ProjectModel>> projectsListings;
 
     List<ProjectModel> projectsList = new ArrayList<>();
 
-    public void setProjectList() {
-
+    MainViewModel(Application application) {
+        super(application);
+        projectsListings = ProjectRepository.getInstance().getProjectList();
     }
 
-    public List<ProjectModel> getProjectsList() {
-        return projectsList;
+    public LiveData<List<ProjectModel>> getProjectsListObservable() {
+        return projectsListings;
     }
+
+
 }
