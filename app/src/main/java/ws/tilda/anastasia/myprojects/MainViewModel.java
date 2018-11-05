0649dfa8ff391068bc8006e3409fd7fc370e025b
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-05 14:50:59 +0200

    Injected ProjectRepository into MainViewModel class.

--- app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
@@ -6,14 +6,20 @@ import android.arch.lifecycle.LiveData;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 public class MainViewModel extends AndroidViewModel {
     public final LiveData<List<ProjectModel>> projectListing;
 
+    @Inject
+    ProjectRepository mProjectRepository;
+
     public MainViewModel(Application application) {
         super(application);
-        projectListing = ProjectRepository.getInstance().getProjectList();
+        projectListing = mProjectRepository.getProjectList();
     }
 
+    // Expose the LiveData Projects query so the UI can observe it.
     public LiveData<List<ProjectModel>> getProjectsListObservable() {
         return projectListing;
     }
