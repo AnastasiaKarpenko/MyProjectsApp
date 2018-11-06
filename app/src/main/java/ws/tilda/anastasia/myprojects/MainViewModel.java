522cc8436b8a621373337aec801e41ecb5cd420c
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-06 11:05:14 +0200

    In the method initializeObserver() we moved the logic of creating projects and adding it one by one from the saveProjectList() method in reactive method onNext(), in method onComplete() we copy the created list of items to the projectList.

--- app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
@@ -9,13 +9,15 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainViewModel extends ViewModel {
 
     List<ProjectModel> projectsList = new ArrayList<>();
+    List<ProjectModel> dummyProjectList = new ArrayList<>();
 
-    private Observable mObservable;
-    private Observer mObserver;
+    private Observable<Integer> mObservable;
+    private Observer<Integer> mObserver;
 
     private void initializeObservable() {
         mObservable = Observable.create(new ObservableOnSubscribe<Integer>() {
@@ -26,13 +28,35 @@ public class MainViewModel extends ViewModel {
                 }
 
                 emitter.onComplete();
-
-
             }
         });
     }
 
+    private void initializeObserver() {
+        mObserver = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
 
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                ProjectModel projectModel = new ProjectModel();
+                projectModel.title = "Project " + integer;
+                dummyProjectList.add(projectModel);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                projectsList.addAll(dummyProjectList);
+            }
+        };
+    }
 
     public void setProjectList() {
         List<ProjectModel> dummyProjectList = new ArrayList<>();
