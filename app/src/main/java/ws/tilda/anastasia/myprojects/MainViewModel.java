b371c2ea000d6aca0c1e2c0000930a2249e8abb2
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-06 10:58:32 +0200

    Created variables for Observable and Observer, implemented method to initialize Observable

--- app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/MainViewModel.java
@@ -5,9 +5,34 @@ import android.arch.lifecycle.ViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+
 public class MainViewModel extends ViewModel {
 
     List<ProjectModel> projectsList = new ArrayList<>();
+
+    private Observable mObservable;
+    private Observer mObserver;
+
+    private void initializeObservable() {
+        mObservable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; i < 5; i++) {
+                    emitter.onNext(i);
+                }
+
+                emitter.onComplete();
+
+
+            }
+        });
+    }
+
+
 
     public void setProjectList() {
         List<ProjectModel> dummyProjectList = new ArrayList<>();
