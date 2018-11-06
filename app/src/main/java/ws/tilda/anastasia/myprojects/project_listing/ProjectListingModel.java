4534226f73853312e9e27f2fa298c889b2196a22
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-06 12:25:29 +0200

    Returning new Observable that will turn each emitted item into an Observable

--- app/src/main/java/ws/tilda/anastasia/myprojects/project_listing/ProjectListingModel.java
+++ app/src/main/java/ws/tilda/anastasia/myprojects/project_listing/ProjectListingModel.java
@@ -1,6 +1,29 @@
 package ws.tilda.anastasia.myprojects.project_listing;
 
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+
 public class ProjectListingModel {
+
+    List<ProjectModel> mProjectModelList;
+
+    Observable<List<ProjectModel>> getProjectListObservable() {
+
+
+        return Observable.range(0, 5)
+                .map(new Function<Integer, List<ProjectModel>>() {
+                    @Override
+                    public List<ProjectModel> apply(Integer integer) throws Exception {
+                        ProjectModel projectModel = new ProjectModel();
+                        projectModel.title = "Project " + integer;
+                        mProjectModelList.add(projectModel);
+
+                        return mProjectModelList;
+                    }
+                });
+    }
 
 
 }
